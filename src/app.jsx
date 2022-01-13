e97fd787f1ab98d13e3023baf9eb0acab8c5f07a
pyeonne <pyeonne@gmail.com>
2022-01-14 00:20:32 +0900

    ⚡️ Handle image uploading

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import styles from './app.module.css';
 import Login from './components/login/login';
 import Maker from './components/maker/maker';
 
-function App({ authService }) {
+function App({ FileInput, authService }) {
     return (
         <div className={styles.app}>
             <BrowserRouter>
@@ -15,7 +15,12 @@ function App({ authService }) {
                     />
                     <Route
                         path="maker"
-                        element={<Maker authService={authService} />}
+                        element={
+                            <Maker
+                                FileInput={FileInput}
+                                authService={authService}
+                            />
+                        }
                     />
                 </Routes>
             </BrowserRouter>
